12db14d787b69d3316d1ac4806cee1036a42ab12
Eric Foucault <efoucault@gmail.com>
2018-08-28 11:38:55 +0200

    add options

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,15 @@
 import "bootstrap";
+import $ from "jquery"
+
+$("[data-form-prepend]").click(function(e) {
+  var obj = $($(this).attr("data-form-prepend"));
+  obj.find("input, select, textarea").each(function() {
+    $(this).attr("name", function() {
+      return $(this)
+        .attr("name")
+        .replace("new_record", new Date().getTime());
+    });
+  });
+  obj.insertBefore(this);
+  return false;
+});
