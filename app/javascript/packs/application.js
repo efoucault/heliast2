d88afd3fb8326f5a5631f835236165e5408986cc
Eric Foucault <efoucault@gmail.com>
2018-08-28 15:13:09 +0200

    can add remove and replaced choose file by camera icon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "bootstrap";
 import $ from "jquery"
 import '../components/category_choice';
@@ -14,4 +13,26 @@ $("[data-form-prepend]").click(function(e) {
   });
   obj.insertBefore(this);
   return false;
+});
+
+
+const pictureIcons = document.querySelectorAll(".fa-camera");
+
+  pictureIcons.forEach((icon) => {
+  icon.addEventListener("click", (event) => {
+    const index = event.currentTarget.dataset.index;
+    console.log(document.getElementById(`question_options_attributes_${index}_photo`));
+    document.getElementById(`question_options_attributes_${index}_photo`).click();
+    // console.log(chooseFile);
+  });
+});
+
+
+const deleteButtons = document.querySelectorAll(".remove");
+
+deleteButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const index = event.currentTarget.dataset.index;
+    document.getElementById(`option_fields_${index}`).style.display = "none";
+  });
 });
