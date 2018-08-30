c9d50e02757f4d3fb7abfe25fa49a71dda643b83
loig kerebel <kerebel.loig@gmail.com>
2018-08-30 17:23:10 +0200

    seeds en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -145,6 +145,10 @@ rocco = User.create(password: '123456', admin: false, email: 'rocco@gmail.com')
 
 p User.all
 
+puts "answer by id"
+
+
+
 puts "categories creation"
 url_look = "https://res.cloudinary.com/offbatt/image/upload/v1535620212/look.jpg"
 look = Category.new(name: "Look")
@@ -179,7 +183,6 @@ question3 = Question.create!(user: eric , description: 'Boire ou conduire ?', ca
 question4 = Question.create!(user: ophelie , description: 'Le suivre ou rester ?', category: achats, type_resolution: "Mode héliaste", deadline: DateTime.now  )
 question5 = Question.create!(user: loig , description: 'Passion ou Raison ?', category: love, type_resolution: "Mode héliaste", deadline: DateTime.now  )
 question6 = Question.create!(user: louis , description: 'Payer un loyer à son partenaire ?', category: love, type_resolution: "Mode héliaste", deadline: DateTime.now )
-
 question7 = Question.create!(user: rose , description: 'Sauver son couple ou suivre un autre homme ?', category: love, type_resolution: "Mode héliaste", deadline: DateTime.now  )
 question8 = Question.create!(user: eva , description: 'Je me pose la question suivante: pour les hommes c est quoi tromper ?', category: love, type_resolution: "Mode héliaste", deadline: DateTime.now )
 question9 = Question.create!(user: ines , description: 'Quel livre me conseiller vous sachant que je suis plutôt fan de science-fiction', category: culture, type_resolution: "Mode héliaste", deadline: DateTime.now  )
@@ -187,53 +190,15 @@ question10 = Question.create!(user: lou , description: 'j hesite entre le robot 
 question11 = Question.create!(user: julia , description: 'Passer de sexfriend a en couple s est possible?', category: love, type_resolution: "Mode héliaste", deadline: DateTime.now  )
 question12 = Question.create!(user: lucie , description: 'Je suis en CDI mais la boite est pas géniale, une amie me propose un cdd dans un cadre de travail magnifique avec des gens ultra competents qui en plus me feront progresser, que faire ? je suis agée de 32 ans et je souhaites encore avancer...', category: travail, type_resolution: "Mode héliaste", deadline: DateTime.now )
 
-question13 = Question.create!(user: lucas , description: 'Quelle couleur me va le mieux ?', category: look, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question14 = Question.create!(user: arthur , description: 'Quel tatouage choisir ?', category: food, type_resolution: "Mode héliaste", deadline: DateTime.now )
-question15 = Question.create!(user: louise , description: 'Boire ou conduire ?', category: culture, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question16 = Question.create!(user: ophelie , description: 'Le suivre ou rester ?', category: achats, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question17 = Question.create!(user: jules , description: 'Passion ou Raison ?', category: love, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question18 = Question.create!(user: gabriel , description: 'Payer un loyer à son partenaire ?', category: travail, type_resolution: "Mode héliaste", deadline: DateTime.now )
+question13 = Question.create!(user: lucas , description: 'Avec un jeans plutôt des baskets ou des chaussures de ville ?', category: look, type_resolution: "Mode héliaste", deadline: DateTime.now  )
+question14 = Question.create!(user: arthur , description: 'Pour le petit déjeuner, plutôt céréales ou croissant ?', category: food, type_resolution: "Mode héliaste", deadline: DateTime.now )
+question15 = Question.create!(user: louise , description: 'Le dernier Mission impossible vaut-il le coup ?', category: culture, type_resolution: "Mode héliaste", deadline: DateTime.now  )
+question16 = Question.create!(user: ophelie , description: 'Je dois changer d ordinateur et j hésite entre un mac et un ordinateur ?', category: achats, type_resolution: "Mode héliaste", deadline: DateTime.now  )
+question17 = Question.create!(user: jules , description: 'Je dois vraiment lui offrir un cadeau pour la saint-valentin ?', category: love, type_resolution: "Mode héliaste", deadline: DateTime.now  )
+question18 = Question.create!(user: gabriel , description: 'Je pense que je ne vais pas rester dans mon travail, je dois plutôt ?', category: travail, type_resolution: "Mode héliaste", deadline: DateTime.now )
 
-question19 = Question.create!(user: mathys , description: 'Quelle couleur me va le mieux ?', category: look, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question20 = Question.create!(user: neo , description: 'Quel tatouage choisir ?', category: food, type_resolution: "Mode héliaste", deadline: DateTime.now )
-question21 = Question.create!(user: clara , description: 'Boire ou conduire ?', category: culture, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question22 = Question.create!(user: louna , description: 'Le suivre ou rester ?', category: achats, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question23 = Question.create!(user: alexis , description: 'Passion ou Raison ?', category: love, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question24 = Question.create!(user: louis , description: 'Payer un loyer à son partenaire ?', category: travail, type_resolution: "Mode héliaste", deadline: DateTime.now )
-
-question25 = Question.create!(user: agathe , description: 'Quelle couleur me va le mieux ?', category: look, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question26 = Question.create!(user: baptiste , description: 'Quel tatouage choisir ?', category: food, type_resolution: "Mode héliaste", deadline: DateTime.now )
-question27 = Question.create!(user: margot , description: 'Boire ou conduire ?', category: culture, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question28 = Question.create!(user: leon , description: 'Le suivre ou rester ?', category: achats, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question29 = Question.create!(user: aaron , description: 'Passion ou Raison ?', category: love, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question30 = Question.create!(user: giulia , description: 'Payer un loyer à son partenaire ?', category: travail, type_resolution: "Mode héliaste", deadline: DateTime.now )
-
-question31 = Question.create!(user: eden , description: 'Quelle couleur me va le mieux ?', category: look, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question32 = Question.create!(user: nathan , description: 'Quel tatouage choisir ?', category: food, type_resolution: "Mode héliaste", deadline: DateTime.now )
-question33 = Question.create!(user: ambre , description: 'Boire ou conduire ?', category: culture, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question34 = Question.create!(user: paul , description: 'Le suivre ou rester ?', category: achats, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question35 = Question.create!(user: nathan , description: 'Passion ou Raison ?', category: love, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question36 = Question.create!(user: ethan , description: 'Payer un loyer à son partenaire ?', category: travail, type_resolution: "Mode héliaste", deadline: DateTime.now )
-
-question37 = Question.create!(user: timeo , description: 'Quelle couleur me va le mieux ?', category: look, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question38 = Question.create!(user: clement , description: 'Quel tatouage choisir ?', category: food, type_resolution: "Mode héliaste", deadline: DateTime.now )
-question39 = Question.create!(user: rose , description: 'Boire ou conduire ?', category: culture, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question40 = Question.create!(user: theo , description: 'Le suivre ou rester ?', category: achats, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question41 = Question.create!(user: clement , description: 'Passion ou Raison ?', category: love, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question42 = Question.create!(user: lucie , description: 'Payer un loyer à son partenaire ?', category: travail, type_resolution: "Mode héliaste", deadline: DateTime.now )
-
-question43 = Question.create!(user: sacha , description: 'Quelle couleur me va le mieux ?', category: look, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question44 = Question.create!(user: juliette , description: 'Quel tatouage choisir ?', category: food, type_resolution: "Mode héliaste", deadline: DateTime.now )
-question45 = Question.create!(user: capucine , description: 'Boire ou conduire ?', category: culture, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question46 = Question.create!(user: axel , description: 'Le suivre ou rester ?', category: achats, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question47 = Question.create!(user: elise , description: 'Passion ou Raison ?', category: love, type_resolution: "Mode héliaste", deadline: DateTime.now  )
-question48 = Question.create!(user: liam , description: 'Payer un loyer à son partenaire ?', category: travail, type_resolution: "Mode héliaste", deadline: DateTime.now )
 
 p Question.all
-
-
-
-
 
 option1 = Option.create!(titre: "Le bleu", question: question1)
 option2 = Option.create!(titre: "Le rouge", question: question1)
@@ -253,7 +218,6 @@ option15 = Option.create!(titre: "Ni l'un ni l'autre, l'excès ne donne jamais r
 option16 = Option.create!(titre: "Pourquoi pas, mais si c'est temporaire", question: question6)
 option17 = Option.create!(titre: "Surtout pas, je serais un pigeon", question: question6)
 option18 = Option.create!(titre: "L'amour n'a pas de prix, si ca ne coute qu'un loyer ce n'est pas très cher!", question: question6)
-
 option19 = Option.create!(titre: "Sauver son couple", question: question7)
 option20 = Option.create!(titre: "Suivre un autre homme", question: question7)
 option21 = Option.create!(titre: "aucun des deux, l'hésitation laisse penser que dans les 2 cas rien n'est génial", question: question7)
@@ -270,3 +234,34 @@ option31 = Option.create!(titre: "oui", question: question11)
 option32 = Option.create!(titre: "non", question: question11)
 option33 = Option.create!(titre: "CDI c'est plus sûr et peinard", question: question12)
 option34 = Option.create!(titre: "CDD au moins ça laisse des options", question: question12)
+option1 = Option.create!(titre: "Le bleu", question: question13, remote_photo_url: "https://res.cloudinary.com/offbatt/image/upload/v1535620212/love.jpg")
+option2 = Option.create!(titre: "Le rouge", question: question13)
+option3 = Option.create!(titre: "Le rose", question: question13)
+option4 = Option.create!(titre: "Un papillon", question: question14)
+option5 = Option.create!(titre: "Un guépard", question: question14)
+option6 = Option.create!(titre: "Ou plus classique: Maman je t'aime", question: question14)
+option7 = Option.create!(titre: "Boire et confier sa voiture a Sam", question: question15)
+option8 = Option.create!(titre: "Boire un seul verre et rentrer en voiture", question: question15)
+option9 = Option.create!(titre: "Conduire et ramener ses potes bourrés qui vont être relous... super la vie de Sam!!", question: question15)
+option10 = Option.create!(titre: "Le suivre et sacrifier ma vie en abandonnant tout ce que j'ai construit ici: amis, travail..", question: question16)
+option11 = Option.create!(titre: "Le suivre et lui en vouloir toute ma vie mais c'est avec lui que je veux etre quand meme il est vraiment exceptionnel", question: question16)
+option12 = Option.create!(titre: "Rester et pleurer pendant quelques temps avant de me rendre compte qu'il y en a plein d'autres quand meme", question: question16)
+option13 = Option.create!(titre: "Passion: partir faire le tour du monde en voilier", question: question17)
+option14 = Option.create!(titre: "Raison: rester et continuer à travailler pour payer mon loyer", question: question17)
+option15 = Option.create!(titre: "Ni l'un ni l'autre, l'excès ne donne jamais rien de bon", question: question17)
+option16 = Option.create!(titre: "Pourquoi pas, mais si c'est temporaire", question: question18)
+option17 = Option.create!(titre: "Surtout pas, je serais un pigeon", question: question18)
+option18 = Option.create!(titre: "L'amour n'a pas de prix, si ca ne coute qu'un loyer ce n'est pas très cher!", question: question18)
+
+p Answer.all
+
+Answer.new(user: lucie , reponse: 'blablabla', score: 900)
+Answer.new(user: ahmed , reponse: 'ma premiere reponse', score: 800)
+Answer.new(user: lea , reponse: 'j espere que ça marche', score: 700)
+Answer.new(user: ines , reponse: 'en esperant t aider', score: 650)
+Answer.new(user: lou , reponse: 'en cours', score: 990)
+Answer.new(user: lea , reponse: 'bonne chance', score: 1100)
+Answer.new(user: leo , reponse: 'ravie de t avoir aide', score: 750)
+Answer.new(user: jules , reponse: 'sans rancune mais je ne peux rien de plus', score: 900)
+Answer.new(user: gabriel , reponse: 'au revoir et a bientot', score: 300)
+Answer.new(user: arthur , reponse: 'blablabla2', score: 200)
