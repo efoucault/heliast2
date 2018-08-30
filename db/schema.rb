cb3d98787acf418f9557b5444e53ee01a2ec9b21
loig kerebel <kerebel.loig@gmail.com>
2018-08-30 14:34:25 +0200

    nouvelles seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_151602) do
+ActiveRecord::Schema.define(version: 2018_08_30_085937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2018_08_29_151602) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "options", force: :cascade do |t|
