2de65ff993a257bfaab66581fb6a2645b13243fb
Ahmed Menouni <ahmed.menouni@gmail.com>
2018-09-03 16:10:59 +0200

    making action cable works with background job

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -6,4 +6,6 @@ class Notification < ApplicationRecord
   # retrieve all unread notifications from the database.
   scope :unread, -> { where(read_at: nil) }
   scope :recent, ->{ order(created_at: :desc).limit(10) }
+
+  after_commit -> { NotificationRelayJob.perform_later(self) }
 end
